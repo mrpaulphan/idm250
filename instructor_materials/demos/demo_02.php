013ddc6066f77ccdf9acca65ef3b1d3e06a94ffd
MrPaulPhan <paul@mrpaulphan.com>
2018-07-10 20:15:55 -0400

    added week 02 demo

---
+++ instructor_materials/demos/demo_02.php
@@ -0,0 +1,28 @@
+
+<h1>Task</h1>
+<p>Example 1: Use a PHP for Loop to display the array below</p>
+<pre>
+<code>
+$characters = ['Paul', 'Mark', 'Jane'];
+</code>
+</pre>
+<br>
+<p>Example 2: Use a PHP foreach Loop to display the array</p>
+<pre>
+<code>
+$schools = [
+    'Drexel University' => [
+        'type' => 'University',
+        'url'  => 'https://drexel.edu'
+    ],
+    'Temple University'  => [
+        'type' => 'University',
+        'url'  => 'https://www.temple.edu/'
+    ],
+    'University of Pennsylvania' => [
+        'type' => 'University',
+        'url'  => 'https://www.upenn.edu/'
+    ]
+];
+</code>
+</pre>
