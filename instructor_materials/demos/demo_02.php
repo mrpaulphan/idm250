5dc7dbe028c88fd95fb8e01ddf1903d611f745ba
MrPaulPhan <paul@mrpaulphan.com>
2018-07-12 19:46:05 -0400

    Added theme

--- instructor_materials/demos/demo_02.php
+++ instructor_materials/demos/demo_02.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP Excercise</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
 
 <h1>Task</h1>
 <p>Example 1: Use a PHP for Loop to display the array below</p>
@@ -26,3 +36,41 @@ $schools = [
 ];
 </code>
 </pre>
+
+
+<?php
+
+
+echo '<h1>Recreate everything below this line</h1>';
+echo '<hr>';
+echo '<h2>Example 1</h2>';
+$characters = ['Paul', 'Mark', 'Jane'];
+    for ($i = 0; $i < count($characters); $i++) {
+        echo $i . ' is the key and ' . $characters[$i] . ' is the value in the $characters array <br/>';
+    }
+
+    // -----
+    echo '<h2>Example 2</h2>';
+
+    $schools = [
+        'Drexel University' => [
+            'type' => 'University',
+            'url'  => 'https://drexel.edu'
+        ],
+        'Temple University'  => [
+            'type' => 'University',
+            'url'  => 'https://www.temple.edu/'
+        ],
+        'University of Pennsylvania' => [
+            'type' => 'University',
+            'url'  => 'https://www.upenn.edu/'
+        ],
+      ];
+    foreach ($schools as $key => $school) {
+        echo '<p>' . $key . ' is a ' . $school['type'] . ' and the website is ' . $school['url'] . ' </p>';
+    }
+
+?>
+
+</body>
+</html>
--- wordpress/wp-content/themes/idm250/functions.php
+++ wordpress/wp-content/themes/idm250/functions.php
@@ -1,1 +1,2 @@
-<?php
+<?php 
+echo 'functions......';
