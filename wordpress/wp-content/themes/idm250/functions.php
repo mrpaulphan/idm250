2360ecfaca222e833fa113bbc8da46227cc49170
MrPaulPhan <paul@mrpaulphan.com>
2018-07-16 18:34:52 -0500

    moved all assets to /dist/ folder

--- wordpress/wp-content/themes/idm250/functions.php
+++ wordpress/wp-content/themes/idm250/functions.php
@@ -1,1 +1,6 @@
 <?php 
+
+// Theme will only work for WordPress 4.8.2 or later.
+if (version_compare($GLOBALS['wp_version'], '4.8.2', '<')) {
+    die('This theme only works in WordPress 4.8.2 or later. Please Update Wordpress to the latest version.');
+}
