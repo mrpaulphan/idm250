65ed90da8ddec0ea3c1c278fd4a0018bffd51d09
MrPaulPhan <paul@mrpaulphan.com>
2018-07-17 21:54:14 -0400

    Week 2

--- wordpress/wp-content/themes/idm250/functions.php
+++ wordpress/wp-content/themes/idm250/functions.php
@@ -5,6 +5,13 @@ if (version_compare($GLOBALS['wp_version'], '4.9.6', '<')) {
     die('This theme only works in WordPress 4.9.6 or later. ');
 }
 
+/**
+ * This function will register the navigation menus.
+ * You can call display the menu by calling wp_nav_menu() in your templates
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_nav_menu/
+ * @method register_nav_menus
+ */
 function register_menus()
 {
     register_nav_menus([
