7621f69c28703462ca401dfdeaead26be4952e90
MrPaulPhan <paul@mrpaulphan.com>
2018-08-21 12:39:46 -0400

    Adding comments

--- wordpress/wp-content/themes/wp-starter-theme/footer.php
+++ wordpress/wp-content/themes/wp-starter-theme/footer.php
@@ -6,13 +6,14 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.2
  */
 ?>
-
-<?php wp_footer();?>
+<?php
+   /* Always have wp_footer() just before the closing </body>
+    * tag of your theme, or you will break many plugins, which
+    * generally use this hook to reference JavaScript files.
+    */
+    wp_footer();
+?>
 </body>
 </html>
--- wordpress/wp-content/themes/wp-starter-theme/header.php
+++ wordpress/wp-content/themes/wp-starter-theme/header.php
@@ -15,7 +15,14 @@
 <title><?php the_title();?></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<?php wp_head(); ?>
+<?php 
+  /* Always have wp_head() just before the closing </head>
+    * tag of your theme, or you will break many plugins, which
+    * generally use this hook to add elements to <head> such
+    * as styles, scripts, and meta tags.
+    */
+  wp_head();
+?>
 </head>
 
 <body <?php body_class();?>
