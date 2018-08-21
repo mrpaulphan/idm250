e24e73d4498612302196b57ddb3d34f9a8dd6038
MrPaulPhan <paul@mrpaulphan.com>
2018-08-21 18:29:42 -0400

    starter updates

--- wordpress/wp-content/themes/class_demo/archive.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/**
- * The template for displaying archive pages
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- */
-die('test');
-get_template_part('templates/template-archive');
--- wordpress/wp-content/themes/wp-starter-theme/front-page.php
+++ wordpress/wp-content/themes/wp-starter-theme/front-page.php
@@ -4,10 +4,21 @@
  *
  * If the user has selected a static page for their homepage, this is what will
  * appear.
- * Learn more: https://codex.wordpress.org/Template_Hierarchy
+ * @link: https://codex.wordpress.org/Template_Hierarchy
  *
  */
+
 get_header(); ?>
+		<?php
+        // Show the selected front page content.
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post();
+            endwhile;
+        else :
+            echo 'No Content';
+        endif;
+        ?>
 
-
-<?php get_footer();
+<?php
+get_footer();
