af2eddc79aaad6e2210eb8463c3a4997df203bf1
Paul Phan <paul@happycog.com>
2022-03-07 18:00:16 -0500

    Work in progress

--- public/wp-content/themes/idm250/template-project-listing.php
+++ public/wp-content/themes/idm250/template-project-listing.php
@@ -10,11 +10,15 @@
   <div class="grid-3">
     <?php
       // https://developer.wordpress.org/reference/classes/wp_query/
+      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
       $arg = [
           'post_type' => 'idm-projects',
           'post_status' => 'publish',
           'posts_per_page' => 9,
-          'order' => 'DESC'
+          'order' => 'DESC',
+          'paged' => $paged
+
       ];
       $project_query = new WP_Query($arg);
 
@@ -23,6 +27,15 @@
         while ($project_query->have_posts()) : $project_query->the_post();
             get_template_part('components/project-teaser');
         endwhile;
+        $big = 999999999; // need an unlikely integer
+        echo paginate_links([
+            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+            'format' => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total' => $project_query->max_num_pages
+        ]);
+        echo '</nav>';
+
         // After looping through a separate query, this function restores the $post global to the current post in the main query.
         wp_reset_postdata();
       ?>
