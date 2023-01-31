2126ca6642b2d9ee202b46170da5f9b56d3f19e6
Paul <paul@mrpaulphan.com>
2023-01-31 18:12:28 -0500

    Work in progress

--- wp-content/themes/idm250-theme-02/index.php
+++ wp-content/themes/idm250-theme-02/index.php
@@ -1,3 +1,12 @@
-<?php get_header();?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ **/
+get_header(); ?>
 <h1>Hello</h1>
-<?php get_footer();?>
+<?php get_footer(); ?>
