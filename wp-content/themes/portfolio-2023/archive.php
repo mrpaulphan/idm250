6417f03ac669569a42c9566ad2a18d11010ac2b6
Paul <paul@mrpaulphan.com>
2023-02-27 17:50:40 -0500

    Work in progress

--- wp-content/themes/portfolio-2023/archive.php
+++ wp-content/themes/portfolio-2023/archive.php
@@ -1,26 +1,28 @@
-<?php get_header();
-$title = get_the_archive_title();
-$current_page_number = get_query_var('paged') ? get_query_var('paged') : 1;
-$arg = [
-    'post_type' => 'post',
-    'posts_per_page' => -1,
-    'paged' => $current_page_number];
-$wp_query = new WP_Query($arg);
+<?php
+ get_header();
+ $title = get_the_archive_title();
+ // Get the current page number from the URL. Example ?paged=2
+ if (get_query_var('paged')) {
+     $current_page_number = get_query_var('paged');
+ } else {
+     $current_page_number = 1;
+ }
+ $arg = [
+     'post_type' => 'post', // the post type
+     'posts_per_page' => 10, // number of posts per page
+     'paged' => $current_page_number //
+ ];
 
-//
-$description = get_the_archive_description();
-
-echo $title;
-// loop through $wp_query and output the posts
-if ($wp_query->have_posts()) {
-    while ($wp_query->have_posts()) {
-        $wp_query->the_post();
-        get_template_part('components/card');
-    }
-}
-?>
-
-
+ $wp_query = new WP_Query($arg);
+ $description = get_the_archive_description();
+ // loop through $wp_query and output the posts
+ if ($wp_query->have_posts()) {
+     while ($wp_query->have_posts()) {
+         $wp_query->the_post();
+         get_template_part('components/card');
+     }
+ }
+ ?>
 
 
 <?php get_footer(); ?>
--- wp-content/themes/portfolio-2023/components/card.php
+++ wp-content/themes/portfolio-2023/components/card.php
@@ -1,7 +1,11 @@
 <div class="flex">
+  <?php if (get_the_post_thumbnail_url()) {
+      echo "<div class='w-100'>
+      <img src='" . get_the_post_thumbnail_url() . "' alt='Hero Image'>
+      </div>";
+  } ?>
   <div>
-    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Hero Image">
+    <h1> <?php echo get_the_title(); ?></h1>
+    <p><?php echo get_the_excerpt(); ?></p>
   </div>
-  <h1> <?php echo get_the_title(); ?></h1>
-  <p><?php echo get_the_excerpt(); ?></p>
 </div>
